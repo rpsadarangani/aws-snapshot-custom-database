8c19c70bcc51b2ff482864bc7571cc44567aaf24
Rahul Sadarangani <rps11997@live.in>
2018-12-08 22:59:21 +0530

    Init Commit

---
+++ app.py
@@ -0,0 +1,52 @@
+import boto3
+import paramiko
+import os
+
+client = boto3.client('ec2',)
+
+# This method get all instance with Tag Name Backup
+reservations = client.describe_instances(Filters=[{
+    'Name': 'tag:Backup',
+    'Values': ['Yes']
+}]).get('Reservations', [])
+
+instances = sum([[i for i in r['Instances']] for r in reservations], [])
+
+totalNumberofInstance = len(instances)
+print(f'Total Number of Instance to Backup {totalNumberofInstance}')
+
+# Initialised Paramiko SSH Client
+keySSH = paramiko.RSAKey.from_private_key_file(
+    "Path to your Pem Key or SSH Key")
+connectParmiko = paramiko.SSHClient()
+connectParmiko.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+for instance in instances:
+    print(instance)
+    # This Loop will get Username of Instance Database by looping over EC2 Tags
+    for tag in instance['Tags']:
+        if tag['Key'] == 'Username':
+            username = tag.get('Value')
+            print(username)
+    
+    privateIpAddress = instance['PrivateIpAddress'] # Store Private IP Address for SSH to Server
+    publicpAddress = instance['PublicIpAddress'] # Store Public IP Address for SSH to Server
+
+    # Connect to Instance to put database in backup mode
+    connectParmiko.connect(
+        hostname=privateIpAddress, username=username, pkey=keySSH)
+    stdin, stdout, stderr = connectParmiko.exec_command('echo "Hello World"')
+    print(stdout.read())
+
+    # Get volume id of all attached volume to ec2 machine of instance
+    for volume in instance['BlockDeviceMappings']:
+        if volume.get('Ebs', None) is None:
+            continue
+        vol_id = volume['Ebs']['VolumeId']
+        print(vol_id)  # Store Volume id in a varaible
+        snapshot = client.create_snapshot(
+         VolumeId=vol_id, Description='Sample Snapshot') # Calls Aws Snapshot API
+    
+    stdin, stdout, stderr = connectParmiko.exec_command('echo "Hello World"')
+    print(stdout.read())
+    connectParmiko.close()
---
+++ readme.md
@@ -0,0 +1,14 @@
+# AWS EC2 Backup Script
+
+This is a AWS Snapshot script that can be run from a Python Shell. The Dependacy which is need to be install will be:
+
+1. Boto3
+2. Paramiko
+3. PIPENV
+4. AWS Secret Key and Access ID with EC2 Full Access.
+
+## How to use the Script
+
+- Clone the script on Desktop or Virtual Machine.
+- Create a Python 3 environment using PIPENV
+- Install dependency using `pipenv install`
