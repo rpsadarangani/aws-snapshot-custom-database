23946ecf4b41513b0b71a10b4c0ba36bd70331d0
Rahul Sadarangani <rps11997@live.in>
2019-10-19 01:15:51 +0530

    Improve Code Quality

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ for instance in instances:
     # This Loop will get Username of Instance Database by looping over EC2 Tags
     for tag in instance['Tags']:
         if tag['Key'] == 'Username':
-            username = tag.get('Value')    
+            username = tag.get('Value')
 
     privateIpAddress = instance['PrivateIpAddress'] # Store Private IP Address for SSH to Server
     publicpAddress = instance['PublicIpAddress'] # Store Public IP Address for SSH to Server
@@ -32,7 +32,7 @@ for instance in instances:
     # Connect to Instance to put database in backup mode
     connectParmiko.connect(
         hostname=privateIpAddress, username=username, pkey=keySSH)
-    stdin, stdout, stderr = connectParmiko.exec_command('echo "Hello World"')
+    stdin, stdout, stderr = connectParmiko.exec_command()
 
     # Get volume id of all attached volume to ec2 machine of instance
     for volume in instance['BlockDeviceMappings']:
@@ -41,6 +41,6 @@ for instance in instances:
         vol_id = volume['Ebs']['VolumeId']
         # Calls Aws Snapshot API
         snapshot = client.create_snapshot(
-         VolumeId=vol_id, Description='Sample Snapshot') 
+         VolumeId=vol_id, Description='Sample Snapshot')
     stdin, stdout, stderr = connectParmiko.exec_command()
     connectParmiko.close()
